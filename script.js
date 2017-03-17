4d4d187b7816e3d0a06064cd98019cdae88eb47b
gloiremao <gloiremao@gmail.com>
2017-03-18 01:51:55 +0800

    update web with new design

--- script.js
+++ script.js
@@ -25,29 +25,27 @@ $(document).ready(function(){
     	$('#css-bar').animate({width:'85%'},1500);
     	$('#js-bar').animate({width:'75%'},1500);
     	$('#jq-bar').animate({width:'75%'},1500);
-    	$('#php-bar').animate({width:'70%'},1500);
+    	$('#php-bar').animate({width:'60%'},1500);
     	$('#sql-bar').animate({width:'65%'},1500);
-    	$('#bs-bar').animate({width:'60%'},1500);
+    	$('#bs-bar').animate({width:'90%'},1500);
     	$('#rwd-bar').animate({width:'60%'},1500);
     	$('#ruby-bar').animate({width:'50%'},1500);
     });
 
     $('#java-bar').appear(function() {
     	$('#c-bar').animate({width:'90%'},1500);
-    	$('#cpp-bar').animate({width:'40%'},1500);
-    	$('#java-bar').animate({width:'80%'},1500);
-    	$('#cs-bar').animate({width:'20%'},1500);
+    	$('#cpp-bar').animate({width:'70%'},1500);
+    	$('#java-bar').animate({width:'70%'},1500);
+    	$('#cs-bar').animate({width:'50%'},1500);
     	$('#ooc-bar').animate({width:'50%'},1500);
     });
 
-    $('#ios-bar').appear(function() {
-    	$('#a-bar').animate({width:'30%'},1500);
-    	$('#ios-bar').animate({width:'55%'},1500);
+    $('#a-bar').appear(function() {
+    	$('#a-bar').animate({width:'70%'},1500);
     });
 
-    $('#ps-bar').appear(function() {
-    	$('#ps-bar').animate({width:'10%'},1500);
-    	$('#ms-bar').animate({width:'95%'},1500);
+    $('#ms-bar').appear(function() {
+    	$('#ms-bar').animate({width:'80%'},1500);
     });
 
     $(".bg").interactive_bg({
